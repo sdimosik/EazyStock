c82a2b4d3c74297236bd1f689362bec3edee1b1b
Shabinsky Dmitry <sdimosikvip@gmail.com>
2022-02-23 20:11:11 +0300

    Revert

--- domain/src/main/java/com/sdimosikvip/domain/repository/StockRepository.kt
+++ domain/src/main/java/com/sdimosikvip/domain/repository/StockRepository.kt
@@ -1,30 +1,18 @@
 package com.sdimosikvip.domain.repository
 
-import com.sdimosikvip.domain.models.FavouriteTickerDomain
+import com.sdimosikvip.domain.models.FavouriteStocksDomain
 import com.sdimosikvip.domain.models.StockItemDomain
 import kotlinx.coroutines.flow.Flow
 
 interface StockRepository {
 
-    fun updateOrLoadStocks(
+    suspend fun getStocks(
         tickers: List<String>
     ): Flow<MutableList<StockItemDomain>>
 
-    suspend fun saveStock(stockItemDomain: StockItemDomain)
+    suspend fun saveFavouriteStock(ticker: String)
 
-    suspend fun deleteSaveStock(stockItemDomain: StockItemDomain)
+    suspend fun deleteFavouriteStock(ticker: String)
 
-    suspend fun getCacheStocks(): List<StockItemDomain>
-
-    fun getCacheStocksFlow(): Flow<List<StockItemDomain>>
-
-    fun getCacheStocksFlow(listTicker: List<String>): Flow<List<StockItemDomain>>
-
-    fun getFavouriteStocksFlow(): Flow<List<StockItemDomain>>
-
-    fun getFavouriteTickersFlow(): Flow<List<FavouriteTickerDomain>>
-
-    suspend fun saveFavouriteTickers(favouriteTickerDomain: FavouriteTickerDomain)
-
-    suspend fun deleteFavouriteTickers(favouriteTickerDomain: FavouriteTickerDomain)
+    suspend fun getFavouriteStocks(): Flow<List<FavouriteStocksDomain>>
 }
